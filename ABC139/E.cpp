089e2d8ced4e6c21c5ad508c53c925a532e37b73
thortoyo <thor.toyo@gmail.com>
2019-09-04 23:00:49 +0900

    ABC139_E

--- ABC139/E.cpp
+++ ABC139/E.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 vector< queue<int> > A(1000);
-set<int> done;
+set<int> q, q2;
 
 int main() {
   int64_t N;
@@ -14,40 +14,42 @@ int main() {
       cin >> ain;
       A[i].emplace( --ain );
     }
+    q.emplace(i);
   }
 
+  int M = N * (N-1) / 2;
   int d = 0;
-  while(1) {
+  while( !q.empty() ) {
 //    cout << d << endl;
     int hit = 0;
-    for(int i=0; i<N;i++ ) {
-      if ( done.count(i) != 0 ) continue;
-      if ( A[i].empty() ) continue;
+//    cout << "- "; for( auto i: q ) cout << i << " "; cout << endl;
+
+    while( !q.empty() ) {
+      int i = *( q.begin() );
+//      cout << i << ":";
+      q.erase(i);
+//      cout << "-- "; for( auto i: q ) cout << i << " "; cout << endl;
       int tgt = A[i].front();
-      if ( done.count(tgt) != 0 ) continue;
-//      cout << i << " " << tgt << " " <<  A[tgt].front() <<endl;
-      if( !A[tgt].empty() && A[tgt].front() == i ) {
+//      cout << tgt << " " <<  A[tgt].front() <<endl;
+      if( !A[tgt].empty() && A[tgt].front() == i && (q2.count(tgt)==0)) {
 //        cout <<  "  Hit" << endl;
-        done.emplace(i);
-        done.emplace(tgt);
-//        for( auto aa: done ) cout << aa << ":" << endl;
         A[i].pop();
         A[tgt].pop();
-        hit = 1;
+        q.erase(tgt);
+//        cout << "-- "; for( auto i: q ) cout << i << " "; cout << endl;
+        if ( !A[i].empty() ) q2.emplace(i);
+        if ( !A[tgt].empty() ) q2.emplace(tgt);
+//        for( auto aa: q2 ) cout << aa << ":" << endl;
+        M--;
       }
-    }
-    if ( hit == 0 ) {
-      for( int i=0; i<N; ++i) {
-        if ( !A[i].empty() ) {
-          cout << "-1" << endl;
-          return 0;
-        }
-      }
-      break;
     }
     d++;
-    done.clear();
+    if ( M==0) break;
+    q.clear();
+    swap(q,q2);
+    q2.clear();
   }
+  if (M) d = -1;
 
   cout << d << endl;
 

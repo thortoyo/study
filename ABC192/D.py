25794c328a89c46c6fe444ae00998fe9abb94749
thortoyo <thor.toyo@gmail.com>
2021-02-24 06:17:36 +0900

    ABBC192

--- ABC192/D.py
+++ ABC192/D.py
@@ -3,18 +3,24 @@ M=int(input())
 xmax = 0
 for i in range(len(X)):
   xmax = max(xmax, int(X[i]))
-l = xmax
-r = M+1
-while l+1<r:
-  m = (l+r)//2
-  num = 0
-  base = 1
-  for i in range(len(X)):
-    num += int(X[len(X)-i-1]) * base;
-    base *= m
-#    print(m,num,base)
-  if M >= num:
-    l = m
+if len(X)==1:
+  if xmax <= M:
+    print("1")
   else:
-    r = m
-print(l-xmax)
+    print("0")
+else:
+  l = xmax
+  r = M+1
+  while l+1<r:
+    m = (l+r)//2
+    num = 0
+    base = 1
+    for i in range(len(X)):
+      num += int(X[len(X)-i-1]) * base;
+      base *= m
+      #    print(m,num,base)
+    if M >= num:
+      l = m
+    else:
+      r = m
+  print(l-xmax)
--- ABC192/E.cpp
+++ ABC192/E.cpp
@@ -5,42 +5,47 @@ using namespace std;
 using namespace atcoder;
 typedef long long ll;
 
-bool seen[2002];
-ll T[100001];
-ll K[100001];
 ll tim[100001];
 vector< vector< tuple<int,ll,ll> > > to(100001);
 
-ll minc;
-int goal;
-
-  
-void solve() {
+ll solve() {
   int N, M, X, Y;
-  X--; Y--;
   cin >> N >> M >> X >> Y;
+  X--; Y--;
   REP(i,M) {
     int a,b; ll t, k;
     cin >> a >> b >> t >> k;
     a--; b--;
     to[a].emplace_back(b,t,k);
     to[b].emplace_back(a,t,k);
-    
   }
-  priorty_queue<pair<ll, int> > q;
+  REP(i,N) tim[i] = 1123456789012LL;
+  priority_queue< pair<ll, int> > q;
   q.emplace(0,X);
-  REP(j,N) seen[j] = false;
+  tim[X] = 0;
   while( !q.empty()) {
-    auto v = q.last(); q.pop();
-    for( auto nv : to[v] ) {
-      
-      tim[v] + K[v]
-      if ( tim[nv] > 
-    
-  cout << minc << endl;
+    auto v = q.top(); q.pop();
+    int city = v.second;
+    if (city == Y) {
+      cout << tim[Y] << endl;
+      return tim[Y];
+    }
+    for( auto nv : to[city] ) {
+      int ncity; ll nt,nk,nnt;
+      tie(ncity, nt, nk) = nv;
+      nnt = tim[city] + nt + (nk - (tim[city]%nk)) % nk;
+      if ( tim[ncity] <= nnt ) continue;
+      tim[ncity] = nnt;
+      q.emplace(nnt, ncity);
+    }
+  }
+  if (tim[Y] == 1123456789012LL ) {
+    return -1;
+  } else {
+    return tim[Y];
   }
 }
 
 int main(){
-  solve();
+  cout << solve() << endl;
 }
